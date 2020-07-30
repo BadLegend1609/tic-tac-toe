a21c82a77f4cd06935e422bf013ccc8e0b8c9c79
Busy Bee <pratiksmnth1234@gmail.com>
2020-07-30 15:58:17 +0530

    Completed the game

--- src/partials/board.jsx
+++ src/partials/board.jsx
@@ -16,13 +16,30 @@ function Board() {
 		NaN,
 		NaN,
 	]);
-
+	let message = '';
+	let h = false;
 	const togglePlayer = () => {
 		setPlayer(!player);
 	};
+	if (
+		(squares[0] === squares[1] && squares[0] === squares[2]) ||
+		(squares[3] === squares[4] && squares[3] === squares[5]) ||
+		(squares[6] === squares[7] && squares[6] === squares[8]) ||
+		(squares[0] === squares[4] && squares[0] === squares[8]) ||
+		(squares[2] === squares[4] && squares[2] === squares[6]) ||
+		(squares[0] === squares[3] && squares[0] === squares[6]) ||
+		(squares[1] === squares[4] && squares[1] === squares[7]) ||
+		(squares[2] === squares[5] && squares[2] === squares[8])
+	) {
+		message = 'Player ' + (player ? 2 : 1) + ' wins! 🥳 🥳 ';
+		h = true;
+	}
 	return (
 		<div>
-			<div className='board'>
+			<center>
+				<h1>{message}</h1>
+			</center>
+			<div className='board' hidden={h}>
 				{squares.map((a, i) => (
 					<Box
 						player={player}
@@ -34,7 +51,7 @@ function Board() {
 					/>
 				))}
 			</div>
-			<h3>Player {player ? 1 : 2}'s Turn</h3>
+			<h3 hidden={h}>Player {player ? 1 : 2}'s Turn</h3>
 		</div>
 	);
 }
